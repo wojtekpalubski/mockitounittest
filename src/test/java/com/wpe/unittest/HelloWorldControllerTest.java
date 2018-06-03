911c4e68d4fb6fbba630c58cc4575f561ad303d9
qq <w@p>
2018-06-03 21:55:00 +0200

    test /hello-world

---
+++ src/test/java/com/wpe/unittest/HelloWorldControllerTest.java
@@ -0,0 +1,33 @@
+package com.wpe.unittest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloWorldController.class)
+public class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockmvc;
+
+    @Test
+    public void helloWorld_test() {
+        RequestBuilder request = MockMvcRequestBuilders.get("/hello-world").accept(MediaType.APPLICATION_JSON);
+        try {
+            MvcResult result = mockmvc.perform(request).andReturn();
+            assertEquals("Hello world", result.getResponse().getContentAsString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
